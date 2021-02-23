3e787ba180ef7d798296fe646c1a9e24a8e26eb1
Kirill Savko <kirillsavko25@gmail.com>
2021-02-23 13:02:32 +0300

    feat: add graphql

---
+++ server/server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('../schema/schema');
+
+const app = express();
+const PORT = 4000;
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+app.listen(PORT, err => {
+  err ? console.log(error) : console.log('server started');
+})
